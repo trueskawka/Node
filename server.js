ca31e1be3b47e87396a6f75832e8d2bf897eaf48
Alicja <alicja.raszkowska@gmail.com>
2016-04-05 17:12:50 +0200

    some reactions

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var sys = require("sys"),
-my_http = require("http");
-my_http.createServer(function(request,response){
-    sys.puts("I got kicked");
-    response.writeHeader(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8080);
-sys.puts("Server Running on 8080");
+var http = require("http");
+var url = require('url');
+var server = http.createServer();
+
+server.on('request', function(request,response){
+	var method = request.method;
+	var my_url = request.url;
+	var queryData = url.parse(my_url, true).query;
+	
+	response.writeHeader(200, {"Content-Type": "text/plain"});
+	
+	if (queryData.somekey) {
+		response.end('Data received');
+	} else {
+		response.end('somevalue');
+	}
+	
+}).listen(4000);
+
+console.log("Server Running on 4000");
