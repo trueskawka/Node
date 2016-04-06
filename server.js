c45ce46889100c5f0fafcdfaba70d0c6b43db899
Alicja <alicja.raszkowska@gmail.com>
2016-04-06 11:20:38 +0200

    write to file

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
-var express = require("express");	
+var express = require("express"),
+	fs = require('fs'),
+	url = require('url');	
 var app = express();
-var url = require('url');
 
+var path = __dirname + '/public/data.txt';
 var data = {};
 
+fs.readFile(path, 'utf-8', function(e, rdata){
+	data = JSON.parse(rdata);
+});
+
 app.get('/set', function(request, response) {
-	var my_url = request.url;
-	var queryData = url.parse(my_url, true).query;
-	
+	var queryData = request.query;
+
 	for (var i in queryData) {
 		data[i] = queryData[i];
 	}
 	
-	console.log('set');
-	console.log(data);
-	
+	console.log('set');	
 	response.send();
+		
+	fs.writeFile(path, JSON.stringify(data), function(err) {    
+    	if(err) {
+        	return console.log(err);
+    	}
+    	
+    	console.log("File was saved.");    
+	}); 
 });
 
 app.get('/get', function(request, response) {
-	var my_url = request.url;
-	var queryData = url.parse(my_url, true).query;
+	var queryData = request.query;
 	
-	console.log('get');
-		
+	console.log('get');		
 	response.send(data[queryData['key']]);
 });
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+	console.log('App listening on port 4000.');
 });
